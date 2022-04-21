0f923cc08345e85ae7b76ce5e836b90da8b60cc6
Harkins12345 <49042556+Harkins12345@users.noreply.github.com>
2022-04-21 02:44:10 +0100

    Added basic HTML and frontend JS

--- api/server.js
+++ api/server.js
@@ -4,10 +4,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors('*'));
 app.use(express.json());
+app.use(express.static('../client/static'));
 
 const postRoutes = require('./routes/posts');
 app.use('/posts', postRoutes);
 
-app.get('/', (req, res) => res.send('Welcome to the library'));
+app.get('/', (req, res) => res.sendFile('index.html'));
 
 module.exports = app
